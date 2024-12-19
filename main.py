341814f7c465b34ee7ea717f4390636a78fd4861
Asclepius1 <istileunurik@gmail.com>
2024-12-19 16:36:20 +0500

    fix

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ def test():
  
 if __name__ == "__main__":
     import uvicorn
-    # uvicorn.run("main:app", host="91.147.92.141", reload=True)
-    uvicorn.run("main:app", host="localhost", reload=True)
+    uvicorn.run("main:app", host="91.147.92.141", reload=True)
+    # uvicorn.run("main:app", host="localhost", reload=True)
 
 
