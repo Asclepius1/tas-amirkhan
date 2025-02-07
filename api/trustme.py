eae7b761dacf339d89c9ffb7b475bd285cd2343b
Asclepius1 <istileunurik@gmail.com>
2025-02-07 09:24:04 +0500

    fix

--- api/trustme.py
+++ api/trustme.py
@@ -44,10 +44,8 @@ class ContractUpdate(BaseModel):
 
 
 @router.post("/webhook/test")
-async def webhook_trustme_test(ContractUpdate: ContractUpdate):
+async def webhook_trustme_test(contract_id: str, status: int = 3):
     try:
-        # Получаем данные из тела запроса
-        data_dict = ContractUpdate  # Преобразуем в обычный словарь для удобства
         example = {
             "contract_id": "wriuphbzi",
             "status": 3,
@@ -55,12 +53,12 @@ async def webhook_trustme_test(ContractUpdate: ContractUpdate):
             "contract_url": "www.tct.kz/uploader/wriuphbzi"
         }
 
-        doc_id = data_dict["contract_id"]
+        doc_id = contract_id
         lead_data = search_lead_by_doc_id(doc_id)
 
         lead_id = lead_data['_embedded']['leads'][0]['id']
 
-        if data_dict["status"] == 3:
+        if status == 3:
             upload_signed_doc_in_lead(lead_id, doc_id)    
             return JSONResponse(content={"message": "successful"}, status_code=200)
         return JSONResponse(content={"message": "successful"}, status_code=200)
