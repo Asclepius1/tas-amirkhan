a6cbb409e88f9d0893e5ef64e93d2ce0f3c9fa6a
Asclepius1 <istileunurik@gmail.com>
2025-10-10 01:15:45 +0500

    new endpoint for files

--- api/__init__.py
+++ api/__init__.py
@@ -1,8 +1,10 @@
 from api.amo import router as amo_router
 from api.trustme import router as trustme_router
+from api.files import router as files_router
 from fastapi import APIRouter
 
 
 api_router = APIRouter()
 api_router.include_router(amo_router)
 api_router.include_router(trustme_router)
+api_router.include_router(files_router)
---
+++ api/files.py
@@ -0,0 +1,11 @@
+import re
+from fastapi import APIRouter
+from fastapi.responses import FileResponse
+
+router = APIRouter(prefix="/files", tags=["Files"])
+
+@router.get("download/{file_name}")
+def download_file(file_name: str):
+    file_path = f'temp/{file_name}'
+    orig_name = re.search(r"\/(.*)\[", file_name).group(1)
+    return FileResponse(path=file_path, filename=orig_name, media_type='application/octet-stream')
