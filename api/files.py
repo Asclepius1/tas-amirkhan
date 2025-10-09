e1940421410030e68a91007e68e8327931bd05d3
Asclepius1 <istileunurik@gmail.com>
2025-10-10 02:02:26 +0500

    fix

--- api/files.py
+++ api/files.py
@@ -4,7 +4,7 @@ from fastapi.responses import FileResponse
 
 router = APIRouter(prefix="/files", tags=["Files"])
 
-@router.get("download/{file_name}")
+@router.get("/download/{file_name}")
 def download_file(file_name: str):
     file_path = f'temp/{file_name}'
     # orig_name = re.search(r"(.*)\_\_\_", file_name).group(1)
