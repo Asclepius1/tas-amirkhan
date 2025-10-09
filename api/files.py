339a2100a775d8a4cb0c3c585da948ef62bf0d4d
Asclepius1 <istileunurik@gmail.com>
2025-10-10 01:54:19 +0500

    fix

--- api/files.py
+++ api/files.py
@@ -7,5 +7,5 @@ router = APIRouter(prefix="/files", tags=["Files"])
 @router.get("download/{file_name}")
 def download_file(file_name: str):
     file_path = f'temp/{file_name}'
-    orig_name = re.search(r"\/(.*)\[", file_name).group(1)
-    return FileResponse(path=file_path, filename=orig_name, media_type='application/octet-stream')
+    # orig_name = re.search(r"(.*)\_\_\_", file_name).group(1)
+    return FileResponse(path=file_path, media_type='application/octet-stream')
--- merge_files.py
+++ merge_files.py
@@ -9,21 +9,20 @@ def download_file(file_url: str, format_: str = 'pdf') -> str|None:
 
     response = requests.get(file_url)
     if response.status_code == 200:
-        file_path = f'temp/{uuid.uuid4()}.{format_}'
-        cd = response.headers.get("Content-Disposition", "")
-        filename = None
+        file_path = f'{uuid.uuid4()}.{format_}'
+        # cd = response.headers.get("Content-Disposition", "")
+        # filename = None
 
-        match = re.search(r"filename\*\s*=\s*UTF-8''([^;]+)", cd)
-        if match:
-            filename = urllib.parse.unquote(match.group(1)).split('.')[0]
-            file_path = f'temp/{filename}[{uuid.uuid4()}.{format_}'
-        with open(file_path, 'wb') as f:
+        # match = re.search(r"filename\*\s*=\s*UTF-8''([^;]+)", cd)
+        # if match:
+            # filename = urllib.parse.unquote(match.group(1)).split('.')[0]
+        with open(f"temp/{file_path}", 'wb') as f:
             f.write(response.content)
-        return file_path, filename
+        return file_path
 def merge_files(doc_file_url: str, smeta_file_url:str) -> str:
     
-    doc_file_path, file_name = download_file(doc_file_url)
-    smeta_file_path, _ = download_file(smeta_file_url)
+    doc_file_path = download_file(doc_file_url)
+    smeta_file_path = download_file(smeta_file_url)
 
     if not doc_file_path or not smeta_file_path:
         raise Exception("Ошибка при загрузке файлов")
@@ -32,8 +31,8 @@ def merge_files(doc_file_url: str, smeta_file_url:str) -> str:
     writer = pypdf.PdfWriter(doc_file_path)
     for page in reader.pages:
         writer.add_page(page)
-    result_path = f"temp/{file_name}[{uuid.uuid4()}.pdf"
-    writer.write(result_path)
+    result_path = f'{uuid.uuid4()}.pdf'
+    writer.write(f"temp/{result_path}")
     os.remove(doc_file_path)
     os.remove(smeta_file_path)
     return result_path
