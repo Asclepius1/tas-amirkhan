39fe25210b2ccbd9e6fcbb06761ed68c707339c0
Nursultan <46659405+Asclepius1@users.noreply.github.com>
2025-11-05 07:45:36 +0500

    Update service.py
    Added to trustme url auto sign parameter

--- service.py
+++ service.py
@@ -316,8 +316,7 @@ def trustme_upload_with_file_url(lead_id: str, several_documents: bool = False) 
     tern_off_button(lead_id)
     if several_documents:
         tern_off_button(lead_id, field_id=1334191)
-    url = 'https://test.trustme.kz/trust_contract_public_apis/UploadWithFileURL'
-    print('check - trustme upload start')
+    url = 'https://test.trustme.kz/trust_contract_public_apis/UploadWithFileURL?auto_sign=1'
     # Метод для amo документов 
     files_uuid = get_file_uuid_by_lead_id(lead_id, need_two_files=several_documents)
     amo_files_url = None
